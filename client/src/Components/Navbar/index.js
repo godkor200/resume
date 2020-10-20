8dcfd4027584dec547f2354ee896dbefbaac2ac9
godkor200 <67778293+godkor200@users.noreply.github.com>
2020-10-20 17:22:22 +0900

    add function for ui

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { animateScroll as scroll } from "react-scroll";
+
 import {
   Nav,
   NavbarContainer,
@@ -13,33 +16,84 @@ import {
 } from "./NavbarElement";
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">I am Dreamer</NavLogo>
-          <MobileIcon onClick={toggle}>
-            <FaBars />
-          </MobileIcon>
-          <NavMenu>
-            <NavItem>
-              <NavLinks to="Think">Think Developer</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="AboutMe">About Me</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="Portfolio">Portfolio</NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks to="ContractMe">Contract Me</NavLinks>
-            </NavItem>
-          </NavMenu>
-          <NavBtn>
-            <NavBtnLink to="Needin">Resume</NavBtnLink>
-          </NavBtn>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo to="/" onClick={toggleHome}>
+              Googi
+            </NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks
+                  to="Think"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeClass="active"
+                >
+                  Think Developer
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks
+                  to="AboutMe"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeClass="active"
+                >
+                  About Me
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks
+                  to="Portfolio"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeClass="active"
+                >
+                  Portfolio
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="Footer" smooth={true}>
+                  Contract Me
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/Signin">Resume</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
